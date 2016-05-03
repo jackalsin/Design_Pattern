ff53d56d4eb31639aac4ab043d79282064b67056
jackalsin <jackal.sin@hotmail.com>
2016-05-02 20:22:12 -0400

    Java Reflection Tutor

---
+++ src/Reflection/UFOEnemyShip.java
@@ -0,0 +1,16 @@
+package Reflection;
+
+/**
+ * Created by jacka on 3/24/2016.
+ */
+public class UFOEnemyShip extends EnemyShip {
+
+    /**
+     * The constructor.
+     */
+    public UFOEnemyShip() {
+        setName("UFO Enemy Ship ");
+
+        setAmtDamage(100);
+    }
+}

134f10600dd911e2be0671e8ea59698e40f3a253
Zhiwei Xin <zhixi@microsoft.com>
2021-01-16 22:52:42 -0800

    Reinit

--- docs/conf.py
+++ docs/conf.py
@@ -29,8 +29,7 @@ author = 'Jackal Sin'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-# 'recommonmark'
- "sphinx_rtd_theme"
+'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -39,7 +38,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +46,10 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. Design Pattern documentation master file, created by
-   sphinx-quickstart on Sat Jan 16 20:40:25 2021.
+   sphinx-quickstart on Sat Jan 16 22:47:48 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
